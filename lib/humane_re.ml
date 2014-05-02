e1aa47b2d7835592b32b1228a42f1521b2297f4d
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-05-02 13:54:42 -0400

    move match outside of S sig

--- lib/humane_re.ml
+++ lib/humane_re.ml
@@ -2,23 +2,26 @@ module List = ListLabels
 module String = StringLabels
 module Array = ArrayLabels
 
+module type Match = sig
+  type t
+  type str
+  type index
+
+  val get : t -> index -> str option
+  val get_pos : t -> index -> (int * int) option
+
+  val fold_left : t -> init:'a -> 
+    f:('a -> pos:(int * int) -> str:str -> 'a) -> 'a
+
+  val all_matched : t -> str list
+  val all : t -> str option list
+end
+
 module type S = sig
   type t
   type str
 
-  module Match : sig
-    type t
-    type index
-
-    val get : t -> index -> string option
-    val get_pos : t -> index -> (int * int) option
-
-    val fold_left : t -> init:'a -> 
-      f:('a -> pos:(int * int) -> str:str -> 'a) -> 'a
-
-    val all_matched : t -> string list
-    val all : t -> string option list
-  end
+  module Match : Match with type str = str
 
   val regexp : string -> t
   val quote : str -> t
--- lib/humane_re.mli
+++ lib/humane_re.mli
@@ -1,20 +1,23 @@
+module type Match = sig
+  type t
+  type str
+  type index
+
+  val get : t -> index -> str option
+  val get_pos : t -> index -> (int * int) option
+
+  val fold_left : t -> init:'a -> 
+    f:('a -> pos:(int * int) -> str:str -> 'a) -> 'a
+
+  val all_matched : t -> str list
+  val all : t -> str option list
+end
+
 module type S = sig
   type t
   type str
 
-  module Match : sig
-    type t
-    type index
-
-    val get : t -> index -> string option
-    val get_pos : t -> index -> (int * int) option
-
-    val fold_left : t -> init:'a -> 
-      f:('a -> pos:(int * int) -> str:str -> 'a) -> 'a
-
-    val all_matched : t -> string list
-    val all : t -> string option list
-  end
+  module Match : Match with type str = str
 
   val regexp : string -> t
   val quote : str -> t
@@ -35,5 +38,7 @@ module type S = sig
     val (=~) : str -> t -> bool
   end
 end
+
+
 
 (* module String : (module S with str = string) *)
