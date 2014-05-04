30b88867aa2a4d02921e8b0cd9839b433609666b
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-05-04 02:08:30 -0400

    test find matches

--- lib_test/test1.ml
+++ lib_test/test1.ml
@@ -12,10 +12,18 @@ let test_split_simple _ =
   let s' = Str.split re str in
   assert_equal s s' ~printer
 
+let test_find_matches _ =
+  let re = Str.regexp "[0-9]+" in
+  let str = "123 456 789 testing 000" in
+  let s = ["123";"456";"789";"000"] in
+  let s' = Str.find_matches re str in
+  assert_equal s s' ~printer
+
 let test_fixtures =
   "test Humane_re.Str" >:::
   [
     "test split simple" >:: test_split_simple;
+    "test find matches" >:: test_find_matches;
   ]
 
 let _ = run_test_tt_main test_fixtures
