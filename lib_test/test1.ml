fe915552d8ae612b372d71cf609ce9250aa4c402
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-05-04 02:21:05 -0400

    test find concat groups

--- lib_test/test1.ml
+++ lib_test/test1.ml
@@ -19,11 +19,19 @@ let test_find_matches _ =
   let s' = Str.find_matches re str in
   assert_equal s s' ~printer
 
+let test_find_concat_groups _ =
+  let re = Str.regexp "\\([0-9]+\\)_\\([0-9]+\\)" in
+  let str = "123_789 testin one two 000_111 foobar" in
+  let s = ["123"; "789"; "000"; "111"] in
+  let s' = Str.find_concat_groups re str in
+  assert_equal s s' ~printer
+
 let test_fixtures =
   "test Humane_re.Str" >:::
   [
     "test split simple" >:: test_split_simple;
     "test find matches" >:: test_find_matches;
+    "test find concat groups" >:: test_find_concat_groups;
   ]
 
 let _ = run_test_tt_main test_fixtures
