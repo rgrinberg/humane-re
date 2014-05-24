95c47236ed3179028c25d9f24647932da303b331
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-05-24 17:35:47 -0400

    extract links example

--- lib_test/test_blog_post.ml
+++ lib_test/test_blog_post.ml
@@ -28,6 +28,40 @@ let test_emails _ =
   test_cases |> List.iter (fun (email, result) ->
     assert_equal (is_valid_email email) result)
 
+let extract_imgur_links page =
+  let is_imgur s =
+    let open Str.Infix in
+    s =~ (Str.regexp ".+\\bimgur\\.com.+")
+  in
+  let re = Str.regexp "<a href=\"\\([^\"]+\\)\">\\([^<>]+\\)</a>" in
+  page
+  |> Str.fold_left_groups re ~init:[]
+       ~f:(fun acc g ->
+         match Str.Group.all g with
+         | [href; text] when is_imgur href -> (href, text)::acc
+         | _ -> acc)
+  |> List.rev
+
+let list_strings_tuples l =
+  l
+  |> List.map (fun (x, y) -> Printf.sprintf "(%s, %s)" x y)
+  |> String.concat "\n"
+
+let test_extract _ =
+  let page = "
+    <a href=\"http://imgur.com/xxx.jpg\">text 1</a> <a href=\"http://imgur.com/gif.jpg\">text 2</a>
+    <a href=\"http://facebook.com/xxx.gif\">text 3</a>
+    <b>random tag</b>
+    <a href>invalid link</a>
+    <a href=\"http://imgur.com/png.jpg\">imgur</a>" in
+  let links = extract_imgur_links page in
+  assert_equal links ~printer:list_strings_tuples
+    [
+      ("http://imgur.com/xxx.jpg", "text 1");
+      ("http://imgur.com/gif.jpg", "text 2");
+      ("http://imgur.com/png.jpg", "imgur");
+    ]
+
 let test_words _ =
   let words = "Lorem ipsum dolor sit amet, consectetur adipisicing elit,
 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." in
@@ -55,6 +89,7 @@ let test_fixtures =
     "test validate emails" >:: test_emails;
     "test extract words" >:: test_words;
     "test parse headers" >:: test_headers;
+    "test extract links" >:: test_extract;
   ]
 
 let _ = run_test_tt_main test_fixtures
